ef504fb5aaf0b536c2832fb9516f85d46e934671
Dmitriy Pleshevskiy <dmitriy@ideascup.me>
2022-04-05 17:18:23 +0300

    feat(nix/prog): add xh, fd, z, bat

--- nix/home.nix
+++ nix/home.nix
@@ -19,9 +19,16 @@ in
   # changes in each release.
   home.stateVersion = "21.11";
 
+  home.packages = [
+    pkgs.xh           # friendly and fast tool for sending HTTP requests
+    pkgs.fd           # a simple, fast and user-friendly alternative to find
+    pkgs.bat          # a cat clone with syntax highlighting and git integration
+  ];
+
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
+  # enable z shell
   programs.zsh = {
     enable = true;
     enableAutosuggestions = true;
@@ -53,6 +60,7 @@ in
     '';
   };
 
+  # enable git VCS
   programs.git = {
     enable = true;
     userName = secrets.git.name;
@@ -72,9 +80,16 @@ in
     };
   };
 
+  # replacement for 'ls' written in rust
   programs.exa = {
     enable = true;
     enableAliases = true;
+  };
+
+  # a fast cd command that learns your habbits
+  programs.zoxide = {
+    enable = true;
+    enableZshIntegration = true;
   };
 
   xdg.configFile = {
