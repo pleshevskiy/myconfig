2cb468f9a592cae0bacfe199b0be710ef33476c9
Dmitriy Pleshevskiy <dmitriy@ideascup.me>
2022-04-12 23:15:05 +0300

    feat(prog/xmobar): add support muted

--- programs/xmonad/xmobar/scripts/get_volume.sh
+++ programs/xmonad/xmobar/scripts/get_volume.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-echo $(amixer -D pulse sget Master | egrep -o "[0-9]+%" | head -n 1)
+res=$(amixer -D pulse sget Master)
+enabled=$(echo $res | egrep -o "\[on\]" | wc -l)
+if [[ $enabled == "0" ]]; then
+  echo "off"
+else
+  echo $(echo $res | egrep -o "[0-9]+%" | head -n 1)
+fi
 exit 0
