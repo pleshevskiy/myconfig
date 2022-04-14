414f836a3e07b2b6219ad85401a41968b1b8fd95
Dmitriy Pleshevskiy <dmitriy@ideascup.me>
2022-04-14 18:22:19 +0300

    feat: add more wallpapers

--- wallpapers/build.sh
+++ wallpapers/build.sh
@@ -4,7 +4,7 @@ cd -- $(dirname "$0")
 echo "Change dir: $PWD"
 
 jpg_file_names=$(exa -1 | grep jpg)
-jpg_file_count=$(echo $jpg_images | wc -l)
+jpg_file_count=$(exa -1 | grep jpg | wc -l)
 
 if [[ $jpg_file_names != "" && $jpg_file_count != "0" ]]; then
   echo found: $jpg_file_count images
